d162bb85aecd96977ee62c3ca92d2395fecaeb85
Iliass F <89900801+IliFar@users.noreply.github.com>
2022-04-22 22:35:14 +0200

    changed map name

---
+++ src/app/components/main/loadingData/loadingData.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loadingData.css";
+
+function LoadingData() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
+
+export default LoadingData;
